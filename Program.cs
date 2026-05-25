f4eff5e23e5c3871639bf7f62a9ef0df5abcdb51
Malin Boëthius <malin@kruso.se>
2026-05-25 09:01:31 +0200

    add methods

--- Program.cs
+++ Program.cs
@@ -46,4 +46,91 @@ while (runProgram)
 }
     
 
-    
+    //first met
+    static int GetPriceFromAge(int age)
+{
+    if (age < 20)
+    {
+        return 80;
+    }
+    else
+    {
+        if (age > 64)
+        {
+            return 90;
+        }
+        else
+        {
+            return 120;
+        }
+    }
+}
+
+//sec met
+static void CheckAgePrice()
+{
+    Console.Write("Ange ålder: ");
+    string input = Console.ReadLine();
+
+    int age = int.Parse(input);
+    int price = GetPriceFromAge(age);
+
+    if (age < 20)
+    {
+        Console.WriteLine("Ungdomspris: 80kr");
+    }
+    else if (age > 64)
+    {
+        Console.WriteLine("Pensionärspris: 90kr");
+    }
+    else
+    {
+        Console.WriteLine("Standardpris: 120kr");
+    }
+}
+
+
+//third met
+static void CalculateGroupPrice()
+{
+    Console.Write("Hur många personer är ni? ");
+    int numberOfPeople = int.Parse(Console.ReadLine());
+
+    int totalPrice = 0;
+
+    for (int i = 1; i <= numberOfPeople; i++)
+    {
+        Console.Write($"Ange ålder för person {i}: ");
+        int age = int.Parse(Console.ReadLine());
+
+        totalPrice += GetPriceFromAge(age);
+    }
+
+    Console.WriteLine($"Antal personer: {numberOfPeople}");
+    Console.WriteLine($"Totalkostnad: {totalPrice}kr");
+}
+
+//fourth met
+static void RepeatTenTimes()
+{
+    Console.Write("Skriv en text: ");
+    string input = Console.ReadLine();
+
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.Write($"{i}. {input} ");
+    }
+
+    Console.WriteLine();
+}
+
+//
+static void PrintThirdWord()
+{
+    Console.Write("Skriv en mening med minst tre ord: ");
+    string sentence = Console.ReadLine();
+
+    string[] words = sentence.Split(' ');
+
+    Console.WriteLine($"Det tredje ordet är: {words[2]}");
+}
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101b875f7cfa949eb9b6e5203ecc95e487bcc288")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
