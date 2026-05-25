c32c8842339f91ded17df7167b7f1ef17a96d7be
Malin Boëthius <malin@kruso.se>
2026-05-25 08:50:19 +0200

    first commit and first lines of code

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+    Console.WriteLine("\n--- Huvudmeny ---");
+    Console.WriteLine("1. Ungdom eller pensionär");
+    Console.WriteLine("2. Räkna pris för sällskap");
+    Console.WriteLine("3. Upprepa tio gånger");
+    Console.WriteLine("4. Det tredje ordet");
+    Console.WriteLine("0. Avsluta");
+    Console.Write("Välj: ");
