bfaa903db0c2cdc6e52093a717280e5810be5a26
Malin Boëthius <malin@kruso.se>
2026-05-25 16:20:59 +0200

    add tryparse to the first method

--- Program.cs
+++ Program.cs
@@ -67,48 +67,80 @@ while (runProgram)
 }
 
 //sec met
+
 static void CheckAgePrice()
 {
     Console.Write("Ange ålder: ");
     string input = Console.ReadLine();
 
-    int age = int.Parse(input);
-    int price = GetPriceFromAge(age);
+    bool success = int.TryParse(input, out int age);
 
-    if (age < 20)
+    if (success)
     {
-        Console.WriteLine("Ungdomspris: 80kr");
-    }
-    else if (age > 64)
-    {
-        Console.WriteLine("Pensionärspris: 90kr");
+        int price = GetPriceFromAge(age);
+        Console.WriteLine($"Pris: {price}kr");
     }
     else
     {
-        Console.WriteLine("Standardpris: 120kr");
+        Console.WriteLine("Felaktig input. Skriv ett heltal.");
     }
+
+// static void CheckAgePrice()
+// {
+    // Console.Write("Ange ålder: ");
+    // string input = Console.ReadLine();
+    // Console.Write("Ange ålder: ");
+    // string input = Console.ReadLine();
+
+    // bool success = int.TryParse(input, out int age);
+
+    // if (success)
+    // {
+    //     Console.WriteLine($"Du skrev åldern: {age}");
+    // }
+    // else
+    // {
+    //     Console.WriteLine("Du måste skriva ett heltal.");
+    // }
+    // int age = int.Parse(input);
+    // int price = GetPriceFromAge(age);
+
+    // if (age < 20)
+    // {
+    //     Console.WriteLine("Ungdomspris: 80kr");
+    // }
+    // else if (age > 64)
+    // {
+    //     Console.WriteLine("Pensionärspris: 90kr");
+    // }
+    // else
+    // {
+    //     Console.WriteLine("Standardpris: 120kr");
+    // }
+
+
+
+
 }
-
-
-//third met
-static void CalculateGroupPrice()
-{
-    Console.Write("Hur många personer är ni? ");
-    int numberOfPeople = int.Parse(Console.ReadLine());
-
-    int totalPrice = 0;
-
-    for (int i = 1; i <= numberOfPeople; i++)
+    //third met
+    static void CalculateGroupPrice()
     {
-        Console.Write($"Ange ålder för person {i}: ");
-        int age = int.Parse(Console.ReadLine());
+        Console.Write("Hur många personer är ni? ");
+        int numberOfPeople = int.Parse(Console.ReadLine());
 
-        totalPrice += GetPriceFromAge(age);
+        int totalPrice = 0;
+
+        for (int i = 1; i <= numberOfPeople; i++)
+        {
+            Console.Write($"Ange ålder för person {i}: ");
+            int age = int.Parse(Console.ReadLine());
+
+            totalPrice += GetPriceFromAge(age);
+        }
+
+        Console.WriteLine($"Antal personer: {numberOfPeople}");
+        Console.WriteLine($"Totalkostnad: {totalPrice}kr");
     }
-
-    Console.WriteLine($"Antal personer: {numberOfPeople}");
-    Console.WriteLine($"Totalkostnad: {totalPrice}kr");
-}
 
 //fourth met
 static void RepeatTenTimes()
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101b875f7cfa949eb9b6e5203ecc95e487bcc288")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4eff5e23e5c3871639bf7f62a9ef0df5abcdb51")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
