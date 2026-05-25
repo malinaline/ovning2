101b875f7cfa949eb9b6e5203ecc95e487bcc288
Malin Boëthius <malin@kruso.se>
2026-05-25 08:56:40 +0200

    add switch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿bool runProgram = true;
+
+while (runProgram)
+{
+
+
+
     Console.WriteLine("\n--- Huvudmeny ---");
     Console.WriteLine("1. Ungdom eller pensionär");
     Console.WriteLine("2. Räkna pris för sällskap");
@@ -6,3 +12,38 @@
     Console.WriteLine("4. Det tredje ordet");
     Console.WriteLine("0. Avsluta");
     Console.Write("Välj: ");
+
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            CheckAgePrice();
+            break;
+
+        case "2":
+            CalculateGroupPrice();
+            break;
+
+        case "3":
+            RepeatTenTimes();
+            break;
+
+        case "4":
+            PrintThirdWord();
+            break;
+
+        case "0":
+            runProgram = false;
+            Console.WriteLine("Programmet avslutas.");
+            break;
+
+        default:
+            Console.WriteLine("Felaktig input, försök igen.");
+            break;
+    }
+
+}
+    
+
+    
